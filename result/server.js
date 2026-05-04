5d47eda25b26c4df6a218d2c5a27ddf3102b77e1
ajrina3006 <ajrinadhity4@gmail.com>
2026-05-04 10:50:05 +0700

    Update socket connection and database settings
    Refactor socket connection handling and database configuration.

--- result/server.js
+++ result/server.js
@@ -6,72 +6,19 @@ var express = require('express'),
     server = require('http').Server(app),
     io = require('socket.io')(server);
 
+io.set('transports', ['polling']);
+
 var port = process.env.PORT || 4000;
 
-io.on('connection', function (socket) {
-
-  socket.emit('message', { text : 'Welcome!' });
-
-  socket.on('subscribe', function (data) {
-    socket.join(data.channel);
-  });
+io.sockets.on('connection', function (socket) {
+  socket.emit('connected', {});
 });
 
+// Ganti ke endpoint RDS kamu
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
-});
-
-async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
-      if (err) {
-        console.error("Waiting for db");
-      }
-      callback(err, client);
-    });
-  },
-  function(err, client) {
-    if (err) {
-      return console.error("Giving up");
-    }
-    console.log("Connected to db");
-    getVotes(client);
-  }
-);
-
-function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
-    if (err) {
-      console.error("Error performing query: " + err);
-    } else {
-      var votes = collectVotesFromResult(result);
-      io.sockets.emit("scores", JSON.stringify(votes));
-    }
-
-    setTimeout(function() {getVotes(client) }, 1000);
-  });
-}
-
-function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
-
-  result.rows.forEach(function (row) {
-    votes[row.vote] = parseInt(row.count);
-  });
-
-  return votes;
-}
-
-app.use(cookieParser());
-app.use(express.urlencoded());
-app.use(express.static(__dirname + '/views'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
-});
-
-server.listen(port, function () {
-  var port = server.address().port;
-  console.log('App running on port ' + port);
+  host: '<RDS_ENDPOINT>',       // ← ganti ini
+  user: 'admin',
+  password: 'LKSNCC2024',
+  database: 'postgres',
+  port: 5432
 });
